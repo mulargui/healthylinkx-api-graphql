6f1a17fcbe42216ed4dd4e0b460fa72ce6b90568
mulargui <mauricio@ulargui.com>
2019-03-08 19:44:34 -0800

    updated dockerfile base image to alpine, node and yarn

--- k8s/update.bat
+++ k8s/update.bat
@@ -1,8 +1,8 @@
 
 kubectl scale deployment healthylinkx-api-deployment --replicas 0
 
-rem wait till everything is down
-timeout 45
+rem create the containers
+minikube ssh "/home/docker/healthylinkx-api-graphql/docker/container.sh BUILD"
 
 kubectl scale deployment healthylinkx-api-deployment --replicas 1
 
